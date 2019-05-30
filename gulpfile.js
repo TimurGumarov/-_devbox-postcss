8ae2dc9396e629e16055d14caac8171ede875bb5
TimurGumarov <tim@timlion.ru>
2019-05-31 00:44:35 +0300

    PostCSS -> SASS

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,16 @@ const project = JSON.parse(fs.readFileSync('./package.json'));
 const {src, dest, series, parallel, watch} = require('gulp');
 
 const del           = require('del'),
-      gulppug       = require('gulp-pug'),
-      postcss       = require('gulp-postcss'),
-      nested        = require('postcss-nested'),
-      autoprefixer  = require('autoprefixer'),
-      cssnano       = require('cssnano'),
-      sugarss       = require('sugarss'),
-      rename        = require('gulp-rename'),
+      gulpPug       = require('gulp-pug'),
+      gulpSass      = require('gulp-sass'),
+      autoprefixer  = require('gulp-autoprefixer'),
       sourcemaps    = require('gulp-sourcemaps'),
       htmlmin       = require('gulp-htmlmin'),
+      cssnano      = require('gulp-cssnano'),
       uglify        = require('gulp-uglify'),
       imagemin      = require('gulp-imagemin'),
       pngquant      = require('imagemin-pngquant'),
+	  gulpCache     = require('gulp-cache'),
       previewBS     = require('browser-sync').create('preview'),
       buildBS       = require('browser-sync').create('build');
 
@@ -43,7 +41,7 @@ const look = {
         'html':     'src/**/*.html',
         'pug':      'src/**/*.pug',
         'js':       'src/js/**/*.js',
-        'sss':      'src/sss/**/*.sss',
+        'sass':     'src/sass/**/*.sass',
         'css':      'src/**/*.css'
     },
     take = {
@@ -52,12 +50,12 @@ const look = {
         'html':     'src/**/*.html',
         'pug':      'src/**/*.pug',
         'js':       'src/js/**/*.js',
-        'sss':      'src/sss/**/*.sss',
+        'sass':     'src/sass/**/*.sass',
         'css':      'src/css/**/*.css',
         'img':      'src/img/**/*.*',
         'stuff':    [
                     'src/**/*.*',
-                '!src/sss/**',
+                '!src/sass/**',
                 '!src/PUG-includes/**',
                 '!src/img/**',
                 '!src/js/**',
@@ -93,7 +91,7 @@ const look = {
                 '!src/PUG-includes',
                 '!src/PUG-includes/**/*'
             ],
-            'sss':  '!src/sss/**/*',
+            'sass': '!src/sass/**/*',
             'pug':  '!src/*.pug'
         },
         'preview': {
@@ -165,25 +163,21 @@ const html = () => {
 
 const pug = () => {
     return src([take.pug, ...except.src.templates])
-           .pipe(gulppug({
+           .pipe(gulpPug({
               pretty: '	'
            }))
            .pipe(dest(put.pug))
            .pipe(previewBS.stream());
 };
 
-const sss = () => {
-    let plugins = [
-        autoprefixer({browsers: ['last 1 version']}),
-        nested
-    ];
-    return src(take.sss)
-           .pipe(sourcemaps.init())
-           .pipe(postcss(plugins, { parser: sugarss }))
-           .pipe(rename({ extname: '.css' }))
-           .pipe(sourcemaps.write())
-           .pipe(dest(put.css))
-           .pipe(previewBS.stream());
+const sass = () => {
+    return src(take.sass)
+		.pipe(sourcemaps.init())
+		.pipe(gulpSass({indentType: 'tab', indentWidth: 1}).on('error', gulpSass.logError))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(sourcemaps.write())
+		.pipe(dest(put.css))
+		.pipe(previewBS.stream());
 };
 
 const js = () => {
@@ -209,7 +203,7 @@ const del_preview = () => {
 watch(look.pug, pug);
 watch(look.html, html);
 watch(look.php, php);
-watch(look.sss, sss);
+watch(look.sass, sass);
 watch(look.js, js);
 
 //- BUILD ----------------------------------------------------------------------
@@ -260,35 +254,30 @@ const html_build = () => {
 
 const pug_build = () => {
     return src([take.pug, ...except.src.templates])
-           .pipe(gulppug())
+           .pipe(gulpPug())
            .pipe(htmlmin({collapseWhitespace: true}))
            .pipe(dest(build.pug))
            .pipe(buildBS.stream());
 };
 
-const sss_build = () => {
-    let plugins = [
-        autoprefixer({browsers: ['last 1 version']}),
-        nested,
-        cssnano()
-    ];
-    return src(take.sss)
-           .pipe(postcss(plugins, { parser: sugarss }))
-           .pipe(rename({ extname: '.css' }))
-           .pipe(dest(build.css))
-           .pipe(buildBS.stream());
+const sass_build = () => {
+    return src(take.sass)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(cssnano())
+		.pipe(dest(build.css));
 };
 
 const js_build = () => {
     return src(take.js)
            .pipe(uglify())
-           .pipe(gulp.dest(build.js))
+           .pipe(dest(build.js))
            .pipe(buildBS.stream());
 };
 
 const stuff_build = () => {
     return src(take.stuff)
-           .pipe(gulp.dest(build.stuff));
+           .pipe(dest(build.stuff));
 };
 
 const del_build = () => {
@@ -297,29 +286,32 @@ const del_build = () => {
 
 const img_build = () => {
     return src(take.img)
-           .pipe(imagemin({
+           .pipe(cache(imagemin({
                progressive: true,
                svgoPlugins: [{removeViewBox: false}],
                use: [pngquant()],
                interlaced: true
-           }))
-           .pipe(gulp.dest(build.img));
+           })))
+           .pipe(dest(build.img));
 };
 
-const watch_build = () => {
-    watch(look.pug, pug_build);
-    watch(look.html, html_build);
-    watch(look.php, php_build);
-    watch(look.sss, sss_build);
-    watch(look.js, js_build);
-};
+// const watch_build = () => {
+//     watch(look.pug, pug_build);
+//     watch(look.html, html_build);
+//     watch(look.php, php_build);
+//     watch(look.sss, sss_build);
+//     watch(look.js, js_build);
+// };
 
 //- DEV ------------------------------------------------------------------------
-const _preview = series(del_preview, parallel(php, html, pug, sss, js, img, stuff), BS_preview);
+const _preview = series(del_preview, parallel(php, html, pug, sass, js, img, stuff), BS_preview);
 
-const _build = series(del_build, parallel(php_build, html_build, pug_build, sss_build, js_build, img_build, stuff_build), BS_build, watch_build);
+const _build = series(del_build, parallel(php_build, html_build, pug_build, sass_build, js_build, img_build, stuff_build), BS_build);
 
 exports.default     = series(_preview);
 exports.preview     = series(_preview);
 exports.build       = series(_build);
-exports.sss         = series(sss);
+exports.pug         = series(pug);
+exports.pug_build   = series(pug_build);
+exports.sass        = series(sass);
+exports.sass_build  = series(sass_build);
